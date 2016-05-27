ed7a9ec4921c383e230fbaf2bb7b64c816f49346
AbrakaDams <damien.machado33@gmail.com>
2016-05-27 15:03:22 +0200

    Intro

---
+++ test/page-1.php
@@ -0,0 +1,10 @@
+
+<section id="about" class="about-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Partie 1</h1>
+            </div>
+        </div>
+    </div>
+</section>
