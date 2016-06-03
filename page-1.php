db95fee6c2517d80cbfc8e3a13d8b81faba45c5a
AbrakaDams <damien.machado33@gmail.com>
2016-06-03 16:23:47 +0200

    update responsive

--- page-1.php
+++ page-1.php
@@ -4,94 +4,19 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="mod2">Images</h1>
-                <div class="col-xs-1"></div>
-                <div class="col-xs-2">
+                <div class="col-xs-4">
                 	<img class="img" src="img/p1.jpg" alt=""><br>
                 	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
                 </div>
-                <div class="col-xs-2">
+                <div class="col-xs-4">
                 	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-						
+                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>	
                 </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                 <div class="col-xs-1"></div>
-                
-            </div>
-            <br>
-            <div class="col-lg-12">
-                <div class="col-xs-1"></div>
-                <div class="col-xs-2">
+                <div class="col-xs-4">
                 	<img class="img" src="img/p1.jpg" alt=""><br>
                 	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
                 </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-						
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                 <div class="col-xs-1"></div>
-                
-            </div>
-            <br>
-             <div class="col-lg-12">
-                <div class="col-xs-1"></div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-						
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                <div class="col-xs-2">
-                	<img class="img" src="img/p1.jpg" alt=""><br>
-                	<p><strong>Et donc du coup oui et blablblablbabla</strong></p>
-                	
-                </div>
-                 <div class="col-xs-1"></div>
-                
-            </div>
+            </div>       
         </div>
     </div>
 </section>
